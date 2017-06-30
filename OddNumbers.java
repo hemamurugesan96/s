48758f1f77b304e626fd6273e23bdb1c8c604afc
Hema M <hemamurugesanece@gmail.com>
2017-06-30 14:44:30 +0530

    OddNumbers.java

---
+++ OddNumbers.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class OddNumbers
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=20;i++)
+{
+if(i%!=0)
+{
+System.out.print(i);
+}
+}
+}
+}
